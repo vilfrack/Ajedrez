536d771fb251020ae54c5d43b667040453e59fd5
Juan villarroel <vilfrack@users.noreply.github.com>
2018-11-08 00:01:16 -0400

    Se crea tablero

---
+++ Ajedrez/Ajedrez/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Ajedrez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelGame = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // PanelGame
+            // 
+            this.PanelGame.AutoSize = true;
+            this.PanelGame.Location = new System.Drawing.Point(0, 12);
+            this.PanelGame.Name = "PanelGame";
+            this.PanelGame.Size = new System.Drawing.Size(691, 439);
+            this.PanelGame.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 478);
+            this.Controls.Add(this.PanelGame);
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelGame;
+    }
+}
+
---
+++ Ajedrez/Ajedrez/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ajedrez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tablero();
+        }
+
+        private void Tablero()
+        {
+            /*8x8*/
+            bool bColor = true;
+            int x = 0;
+            int y = 0;
+            for (int EjeY = 0; EjeY < 8; EjeY++)
+            {
+                for (int EjeX = 0; EjeX < 8; EjeX++)
+                {
+                    x = x + 82;
+                    PictureBox cuadro = new PictureBox();
+                    cuadro.Name = "cuadro" + EjeX + EjeY;
+                    cuadro.MouseHover += new System.EventHandler(cuadro_MouseHover);
+                    cuadro.Width = 80;
+                    cuadro.Height = 80;
+                    cuadro.Location = new Point(x, y);
+                    if (bColor)
+                    {
+                        cuadro.BackColor = Color.White;
+                        bColor = false;
+                    }
+                    else
+                    {
+                        cuadro.BackColor = Color.Black;
+                        bColor = true;
+                    }
+
+                    PanelGame.Controls.Add(cuadro);
+                }
+                bColor = bColor == true ? false : true;
+
+                y = y + 80;
+                x = 0;
+            }
+        }
+
+        private void cuadro_MouseHover(object sender, System.EventArgs e)
+        {
+            //obtenemos el nombre del control
+            string name = ((PictureBox)sender).Name;
+            MessageBox.Show(name);
+        }
+    }
+}
